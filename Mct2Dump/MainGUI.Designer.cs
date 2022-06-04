5e72a5b54d6398645ed2ba61b3a8fb63dfc330b5
守望时空33 <yanhanhui2001a@163.com>
2022-06-04 15:02:19 +0800

    1.0.0

---
+++ Mct2Dump/MainGUI.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Swsk33.Mct2Dump
+{
+	partial class MainGUI
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainGUI));
+			this.mctToDumpOption = new System.Windows.Forms.RadioButton();
+			this.dumpToMctOption = new System.Windows.Forms.RadioButton();
+			this.openInputFile = new System.Windows.Forms.Button();
+			this.inputFilePath = new System.Windows.Forms.TextBox();
+			this.inputLabel = new System.Windows.Forms.Label();
+			this.setSaveFile = new System.Windows.Forms.Button();
+			this.outputLabel = new System.Windows.Forms.Label();
+			this.outputFilePath = new System.Windows.Forms.TextBox();
+			this.convert = new System.Windows.Forms.Button();
+			this.tip = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// mctToDumpOption
+			// 
+			this.mctToDumpOption.AutoSize = true;
+			this.mctToDumpOption.Checked = true;
+			this.mctToDumpOption.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.mctToDumpOption.Location = new System.Drawing.Point(23, 21);
+			this.mctToDumpOption.Name = "mctToDumpOption";
+			this.mctToDumpOption.Size = new System.Drawing.Size(144, 18);
+			this.mctToDumpOption.TabIndex = 0;
+			this.mctToDumpOption.TabStop = true;
+			this.mctToDumpOption.Text = "mct/txt转dump/bin";
+			this.mctToDumpOption.UseVisualStyleBackColor = true;
+			// 
+			// dumpToMctOption
+			// 
+			this.dumpToMctOption.AutoSize = true;
+			this.dumpToMctOption.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.dumpToMctOption.Location = new System.Drawing.Point(173, 21);
+			this.dumpToMctOption.Name = "dumpToMctOption";
+			this.dumpToMctOption.Size = new System.Drawing.Size(144, 18);
+			this.dumpToMctOption.TabIndex = 0;
+			this.dumpToMctOption.Text = "dump/bin转mct/txt";
+			this.dumpToMctOption.UseVisualStyleBackColor = true;
+			// 
+			// openInputFile
+			// 
+			this.openInputFile.Location = new System.Drawing.Point(242, 54);
+			this.openInputFile.Name = "openInputFile";
+			this.openInputFile.Size = new System.Drawing.Size(75, 23);
+			this.openInputFile.TabIndex = 1;
+			this.openInputFile.Text = "浏览";
+			this.openInputFile.UseVisualStyleBackColor = true;
+			this.openInputFile.Click += new System.EventHandler(this.openInputFile_Click);
+			// 
+			// inputFilePath
+			// 
+			this.inputFilePath.Location = new System.Drawing.Point(103, 54);
+			this.inputFilePath.Name = "inputFilePath";
+			this.inputFilePath.Size = new System.Drawing.Size(133, 21);
+			this.inputFilePath.TabIndex = 2;
+			// 
+			// inputLabel
+			// 
+			this.inputLabel.AutoSize = true;
+			this.inputLabel.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.inputLabel.Location = new System.Drawing.Point(17, 58);
+			this.inputLabel.Name = "inputLabel";
+			this.inputLabel.Size = new System.Drawing.Size(91, 14);
+			this.inputLabel.TabIndex = 3;
+			this.inputLabel.Text = "待转换文件：";
+			// 
+			// setSaveFile
+			// 
+			this.setSaveFile.Location = new System.Drawing.Point(242, 86);
+			this.setSaveFile.Name = "setSaveFile";
+			this.setSaveFile.Size = new System.Drawing.Size(75, 23);
+			this.setSaveFile.TabIndex = 1;
+			this.setSaveFile.Text = "浏览";
+			this.setSaveFile.UseVisualStyleBackColor = true;
+			this.setSaveFile.Click += new System.EventHandler(this.setSaveFile_Click);
+			// 
+			// outputLabel
+			// 
+			this.outputLabel.AutoSize = true;
+			this.outputLabel.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.outputLabel.Location = new System.Drawing.Point(31, 89);
+			this.outputLabel.Name = "outputLabel";
+			this.outputLabel.Size = new System.Drawing.Size(77, 14);
+			this.outputLabel.TabIndex = 3;
+			this.outputLabel.Text = "输出位置：";
+			// 
+			// outputFilePath
+			// 
+			this.outputFilePath.Location = new System.Drawing.Point(103, 86);
+			this.outputFilePath.Name = "outputFilePath";
+			this.outputFilePath.Size = new System.Drawing.Size(133, 21);
+			this.outputFilePath.TabIndex = 2;
+			// 
+			// convert
+			// 
+			this.convert.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.convert.Location = new System.Drawing.Point(132, 118);
+			this.convert.Name = "convert";
+			this.convert.Size = new System.Drawing.Size(70, 30);
+			this.convert.TabIndex = 1;
+			this.convert.Text = "转换";
+			this.convert.UseVisualStyleBackColor = true;
+			this.convert.Click += new System.EventHandler(this.convert_Click);
+			// 
+			// tip
+			// 
+			this.tip.AutoSize = true;
+			this.tip.ForeColor = System.Drawing.Color.Red;
+			this.tip.Location = new System.Drawing.Point(3, 144);
+			this.tip.Name = "tip";
+			this.tip.Size = new System.Drawing.Size(71, 12);
+			this.tip.TabIndex = 4;
+			this.tip.Text = "正在转换...";
+			this.tip.Visible = false;
+			// 
+			// MainGUI
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(339, 160);
+			this.Controls.Add(this.tip);
+			this.Controls.Add(this.outputFilePath);
+			this.Controls.Add(this.inputFilePath);
+			this.Controls.Add(this.outputLabel);
+			this.Controls.Add(this.convert);
+			this.Controls.Add(this.setSaveFile);
+			this.Controls.Add(this.inputLabel);
+			this.Controls.Add(this.openInputFile);
+			this.Controls.Add(this.dumpToMctOption);
+			this.Controls.Add(this.mctToDumpOption);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "MainGUI";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "mct和dump互转";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RadioButton mctToDumpOption;
+		private System.Windows.Forms.RadioButton dumpToMctOption;
+		private System.Windows.Forms.Button openInputFile;
+		private System.Windows.Forms.TextBox inputFilePath;
+		private System.Windows.Forms.Label inputLabel;
+		private System.Windows.Forms.Button setSaveFile;
+		private System.Windows.Forms.Label outputLabel;
+		private System.Windows.Forms.TextBox outputFilePath;
+		private System.Windows.Forms.Button convert;
+		private System.Windows.Forms.Label tip;
+	}
+}
+
---
+++ Mct2Dump/MainGUI.cs
@@ -0,0 +1,121 @@
+﻿using Swsk33.ReadAndWriteSharp.FileUtil;
+using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Swsk33.Mct2Dump
+{
+	public partial class MainGUI : Form
+	{
+		public MainGUI()
+		{
+			CheckForIllegalCrossThreadCalls = false;
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// 操纵界面按钮和提示文件自
+		/// </summary>
+		/// <param name="enable">是否全部启用或者禁用，提示文字和按钮相反</param>
+		private void operateButtonsAndTip(bool enable)
+		{
+			openInputFile.Enabled = enable;
+			setSaveFile.Enabled = enable;
+			convert.Enabled = enable;
+			tip.Visible = !enable;
+		}
+
+		private void openInputFile_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog dialog = new OpenFileDialog();
+			if (mctToDumpOption.Checked)
+			{
+				dialog.Filter = "文本数据文件|*.mct;*.txt";
+			}
+			else
+			{
+				dialog.Filter = "二进制数据文件|*.dump;*.bin";
+			}
+			if (dialog.ShowDialog() == DialogResult.OK)
+			{
+				inputFilePath.Text = dialog.FileName;
+			}
+		}
+
+		private void setSaveFile_Click(object sender, EventArgs e)
+		{
+			SaveFileDialog dialog = new SaveFileDialog();
+			if (dumpToMctOption.Checked)
+			{
+				dialog.Filter = "文本数据文件|*.mct;*.txt";
+			}
+			else
+			{
+				dialog.Filter = "二进制数据文件|*.dump;*.bin";
+			}
+			if (dialog.ShowDialog() == DialogResult.OK)
+			{
+				outputFilePath.Text = dialog.FileName;
+			}
+		}
+
+		private void convert_Click(object sender, EventArgs e)
+		{
+			// 输入校验
+			if (!File.Exists(inputFilePath.Text))
+			{
+				MessageBox.Show("指定文件不存在！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+			if (outputFilePath.Text.Equals(""))
+			{
+				MessageBox.Show("输出路径不能为空！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+			if (mctToDumpOption.Checked && BinaryUtils.IsBinaryFile(inputFilePath.Text))
+			{
+				MessageBox.Show("指定的文件不是个文本类型文件！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+			if (dumpToMctOption.Checked && !BinaryUtils.IsBinaryFile(inputFilePath.Text))
+			{
+				MessageBox.Show("指定的文件不是个二进制类型文件！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+			// 执行转换
+			operateButtonsAndTip(false);
+			new Thread(() =>
+			{
+				if (mctToDumpOption.Checked)
+				{
+					byte[] data = ICFileReader.ReadTextToByte(inputFilePath.Text);
+					if (data.Length != 1024)
+					{
+						MessageBox.Show("原文件大小不正确！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+						operateButtonsAndTip(true);
+						return;
+					}
+					BinaryUtils.WriteBinaryFile(outputFilePath.Text, data);
+				}
+				else if (dumpToMctOption.Checked)
+				{
+					string result = null;
+					try
+					{
+						result = ICFileReader.ReadBinaryToText(inputFilePath.Text);
+					}
+					catch (Exception)
+					{
+						MessageBox.Show("原文件大小不正确！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+						operateButtonsAndTip(true);
+						return;
+					}
+					File.WriteAllText(outputFilePath.Text, result);
+				}
+				MessageBox.Show("转换完成！", "完成", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				operateButtonsAndTip(true);
+			}).Start();
+		}
+	}
+}
